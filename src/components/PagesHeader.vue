3094246e68cb1373527941a774a9778526df55dc
Rifat <rifatkhabibulin88@gmail.comgit config --global user.name Rifatgit statusgit config user.email rifatkhabibulin88@gmail.com>
2023-09-17 18:13:25 +0300

    refactor: add router-links

--- src/components/PagesHeader.vue
+++ src/components/PagesHeader.vue
@@ -2,21 +2,19 @@
   <header class="header">
     <div class="header__container container">
       <div class="header__logo">
-        <router-link to="/">
-          <div class="header__logo-content">
-            <h1 class="header__logo-text-title"><span id="logo-text-1">Art</span> Galery</h1>
-            <p class="header__logo-text-sub">SanFrancisco</p>
-            <img src="../assets/logo-border-small.png" alt="logo-border" class="header__logo-border">
-            <img src="../assets/logo-line-small.png" alt="logo-border" class="header__logo-line">
-            <img src="../assets/logo-yellow-small.png" alt="logo-yellow" class="header__logo-yellow">
-          </div>
+        <router-link to="/" class="header__logo-content">
+          <h1 class="header__logo-text-title"><span id="logo-text-1">Art</span> Galery</h1>
+          <p class="header__logo-text-sub">SanFrancisco</p>
+          <img src="../assets/logo-border-small.png" alt="logo-border" class="header__logo-border">
+          <img src="../assets/logo-line-small.png" alt="logo-border" class="header__logo-line">
+          <img src="../assets/logo-yellow-small.png" alt="logo-yellow" class="header__logo-yellow">
         </router-link>
       </div>
       <div class="header__right">
-        <div class="header__search" @click="this.$router.push('/')">
+        <router-link to="/" class="header__search">
           <img src="../assets/search-icon-white.svg" alt="search-icon" class="header__right-icon">
           <p class="header__right-title">Поиск</p>
-        </div>
+        </router-link>
         
         <router-link class="header__favorite-link" to="/favorite">
           <img src="../assets/favorite-icon.png" alt="favorite-icon" class="header__right-icon">
